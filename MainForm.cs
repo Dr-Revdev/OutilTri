eb8fe4aede21cd97fda7ad365d7f406c1118677f
Iwan <iwan.robert.biliec@gmail.com>
2025-09-11 14:31:34 +0200

    init : First commit

---
+++ MainForm.cs
@@ -0,0 +1,13 @@
+using System.Drawing;
+using System.Windows.Forms;
+
+
+public class MainForm : Form
+{
+    public MainForm()
+    {
+        Text = "Tri Download";
+        ClientSize = new Size(900, 520);
+        StartPosition = FormStartPosition.CenterScreen;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace TriDownload;
+
+static class Program
+{
+
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }    
+}
